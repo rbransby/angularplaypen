7a582eff0f3b163e9b8c955868307adb28b8c284
Rob Bransby <rob@bransby.com.au>
2019-11-12 22:58:41 +0800

    messing with table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WpsTableComponent} from './wps-table/wps-table.component';
+import {AppComponent} from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{path: '', component:AppComponent},
+  {path: 'wpstable', component: WpsTableComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WpsTableComponent } from './wps-table/wps-table.component';
+import { MatTableModule } from '@angular/material' 
+import {MatFormFieldModule} from '@angular/material'; 
+import {MatInputModule} from '@angular/material'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WpsTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
